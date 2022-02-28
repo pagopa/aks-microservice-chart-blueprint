b5182bd46b3d135568513c051366c19254b0accf
Matteo Cellucci <matteo.cellucci@keypartner.com>
2022-02-28 14:28:38 +0100

    Add startup to probes and define their standard path

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 80
+const express = require('express');
+const app = express();
+const port = 80;
 
 app.get('/', (req, res) => {
   res.send(`${process.env.USER} : ${process.env.PASS}`);
-})
+});
+
+app.get('/.well-known/live', (req, res) => {
+  res.send('ok');
+});
+
+app.get('/.well-known/ready', (req, res) => {
+  res.send('ok');
+});
+
+app.get('/.well-known/boot', (req, res) => {
+  res.send('ok');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
