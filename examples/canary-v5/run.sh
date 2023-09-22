32e04ed90ff5f37398cf62dce63be87a6c9a4cc9
Jacopo <jacopo.carlini@gmail.com>
2023-09-22 09:14:03 +0200

    merge envconfig

--- examples/canary-v5/run.sh
+++ examples/canary-v5/run.sh
@@ -1,4 +1,4 @@
 rm Chart.lock
 rm -R charts
 helm dependency build
-helm upgrade --namespace apiconfig   --install --values ./values-dev.yaml   --wait --timeout 5m0s  delete-me ./
+helm upgrade --namespace apiconfig  --install --values ./values-dev.yaml   --wait --timeout 5m0s  delete-me-beta ./
