1d2f5bcfa81193648cd857dadcaf04b272d9ca44
Jacopo <jacopo.carlini@gmail.com>
2023-09-22 11:00:05 +0200

    done

--- examples/canary-v5/run.sh
+++ examples/canary-v5/run.sh
@@ -2,3 +2,4 @@ rm Chart.lock
 rm -R charts
 helm dependency build
 helm upgrade --namespace apiconfig  --install --values ./values-dev.yaml   --wait --timeout 5m0s  delete-me-beta ./
+helm upgrade --namespace apiconfig  --install --values ./values-dev.yaml  --set microservice-chart.canaryDelivery.create="false"  --wait --timeout 5m0s  delete-me ./
