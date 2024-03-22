f3ced22651d32b2c8483f488f133962fb748f02f
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-03-22 22:28:13 +0100

    updated canary tests

--- tests/v5-java-helm-complete-test-canary/helm/run.sh
+++ tests/v5-java-helm-complete-test-canary/helm/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../../../scripts/run.sh values-devopslab-dev.yaml blueprint v5-java-helm-complete-test-foo
+../../../scripts/run.sh values-devopslab-dev.yaml blueprint v5-java-helm-complete-test-canary
