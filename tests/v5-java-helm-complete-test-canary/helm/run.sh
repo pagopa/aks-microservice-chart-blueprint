59f94c73c91b9fb4268ea0f49f4974249159db26
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-05-22 15:43:48 +0200

    changed blueprint to testit

--- tests/v5-java-helm-complete-test-canary/helm/run.sh
+++ tests/v5-java-helm-complete-test-canary/helm/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../../../scripts/run.sh values-devopslab-dev.yaml blueprint v5-java-helm-complete-test-canary
+../../../scripts/run.sh values-devopslab-dev.yaml testit v5-java-helm-complete-test-canary
