8c74effec1d1d6d2096fec7ff03db91b9cfa4a00
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-04-17 14:50:04 +0200

    changed tests folders to v7 from v5

--- tests/v7-java-helm-complete-test-bar/helm/deploy.sh
+++ tests/v7-java-helm-complete-test-bar/helm/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../../../scripts/deploy.sh values-devopslab-dev.yaml dvopla-d-itn-dev-aks v5-java-helm-complete-test-bar
+../../../scripts/deploy.sh values-devopslab-dev.yaml dvopla-d-itn-dev-aks v7-java-helm-complete-test-bar
