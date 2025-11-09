20fd23948072b7c79b2220c00505133f0e25f2ad
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-11-09 16:56:42 +0100

    feat: update test names and paths to v8 versions

--- tests/v2-migration-java-helm-complete-test/helm/run-migration.sh
+++ tests/v2-migration-java-helm-complete-test/helm/run-migration.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-../../../scripts/run.sh v7-values-devopslab-dev.yaml testit v2-migration-java-helm-complete-test
+../../../scripts/run.sh v8-values-devopslab-dev.yaml testit v2-migration-java-helm-complete-test
